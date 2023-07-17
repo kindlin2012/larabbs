c7b1e5adb210cc12c794c77b2fd2125935a28cdb
kindlin2012 <kindlin2012@139.com>
2023-07-17 16:29:58 +0000

    回复列表

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -6,6 +6,10 @@ use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Http\Resources\TopicResource;
 use App\Http\Requests\Api\TopicRequest;
+// use Spatie\QueryBuilder\QueryBuilder;
+// use Spatie\QueryBuilder\AllowedFilter;
+use App\Http\Queries\TopicQuery;
+use App\Models\User;
 
 class TopicsController extends Controller
 {
@@ -19,8 +23,101 @@ class TopicsController extends Controller
         return new TopicResource($topic);
     }
 
-    public function index()
+    // public function index(Request $request, Topic $topic)
+    // {
+    //     // $query = $topic->query();
+
+    //     // if ($categoryId = $request->category_id) {
+    //     //     $query->where('category_id', $categoryId);
+    //     // }
+
+    //     // // $topics = $query->withOrder($request->order)->paginate();
+    //     // $topics = $query
+    //     //     ->with('user', 'category')
+    //     //     ->withOrder($request->order)
+    //     //     ->paginate();
+
+    //     //QueryBuilder用法
+    //     $topics = QueryBuilder::for(Topic::class)
+    //         ->allowedIncludes('user', 'category')
+    //         ->allowedFilters([
+    //             'title',
+    //             AllowedFilter::exact('category_id'),
+    //             AllowedFilter::scope('withOrder')->default('recentReplied'),
+    //         ])
+    //         ->paginate();
+
+    //     return TopicResource::collection($topics);
+    // }
+
+    //抽象后调用TopicQuery
+    public function index(Request $request, TopicQuery $query)
     {
-        return 111;
+        $topics = $query->paginate();
+
+        return TopicResource::collection($topics);
+    }
+
+    public function update(TopicRequest $request, Topic $topic)
+    {
+        $this->authorize('update', $topic);
+
+        $topic->update($request->all());
+        return new TopicResource($topic);
+    }
+
+    public function destroy(Topic $topic)
+    {
+        $this->authorize('destroy', $topic);
+
+        $topic->delete();
+
+        return response(null, 204);
+    }
+
+    // public function userIndex(Request $request, User $user)
+    // {
+    //     $query = $user->topics()->getQuery();
+
+    //     $topics = QueryBuilder::for($query)
+    //         ->allowedIncludes('user', 'category')
+    //         ->allowedFilters([
+    //             'title',
+    //             AllowedFilter::exact('category_id'),
+    //             AllowedFilter::scope('withOrder')->default('recentReplied'),
+    //         ])
+    //         ->paginate();
+
+    //     return TopicResource::collection($topics);
+    // }
+    //抽象后
+    public function userIndex(Request $request, User $user, TopicQuery $query)
+    {
+        $topics = $query->where('user_id', $user->id)->paginate();
+
+        return TopicResource::collection($topics);
+    }
+
+    // public function show(Topic $topic)
+    // {
+    //     return new TopicResource($topic);
+    // }
+
+
+    //为了使用 QueryBuilder不使用路由模型绑定
+    // public function show($topicId)
+    // {
+    //     $topic = QueryBuilder::for(Topic::class)
+    //         ->allowedIncludes('user', 'category')
+    //         ->findOrFail($topicId);
+
+    //     return new TopicResource($topic);
+    // }
+
+    //抽象后
+    public function show($topicId, TopicQuery $query)
+    {
+        $topic = $query->findOrFail($topicId);
+        return new TopicResource($topic);
     }
 }
